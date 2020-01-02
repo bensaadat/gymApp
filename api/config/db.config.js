9cdfb0742bbee399feac8de549dca9a5e3ea27d4
bensaadat <amine@goprot.com>
2020-01-02 11:21:02 +0100

    DB Configuration

--- api/config/db.config.js
+++ api/config/db.config.js
@@ -1,6 +1,23 @@
-module.exports = {
+// LOCAL
+// module.exports = {
+//     HOST: "localhost",
+//     USER: "root",
+//     PASSWORD: "",
+//     DB: "goprot.com"
+//   };
+
+  //BETA
+  module.exports = {
     HOST: "localhost",
-    USER: "root",
-    PASSWORD: "",
-    DB: "goprot.com"
+    USER: "goprot",
+    PASSWORD: "cNHiiAE2VG-N68jWiNBBH",
+    DB: "goprotco_bda"
   };
+
+  // PROD
+  // module.exports = {
+  //   HOST: "localhost",
+  //   USER: "goprot",
+  //   PASSWORD: "cNHiiAE2VG-N68jWiNBBH",
+  //   DB: "goprot_prod"
+  // };
